99e227d8f490aae3ba38a464ca02f0981dc5b550
sygnowsk <mariusz.sygnowski@gmail.com>
2016-04-12 13:04:45 +0200

    maps

---
+++ maps/maps.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	weekDay := map[int]string {
+		0 : "Niedziela",
+		1 : "Poniedziałek",
+		2 : "Wtorek",
+		3 : "Sroda",
+		4 : "Czwartek",
+		5 : "Piątek",
+		6 : "Sobota",
+	}
+
+	for key, value := range weekDay {
+		fmt.Printf("key: %d, value : %s\n", key, value)
+
+	}
+
+
+}
